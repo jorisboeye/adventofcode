de5ca6122de73a162a4108fbd9b1d42f442b81d5
Joris Boeye <joris.boeye@zf.com>
2019-12-14 15:10:45 +0100

    initial commit

---
+++ day_4.py
@@ -0,0 +1,18 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.3.0
+#   kernelspec:
+#     display_name: Python (development)
+#     language: python
+#     name: development
+# ---
+
+# %% [markdown]
+# # Advent of code day 4
+
+# %%
